0ce82c3d453ec789b8f5a2761fdda95e1b148aff
Albert Tom <shadowseer99@aol.com>
2015-12-03 11:09:53 -0700

    mesh and button

---
+++ Assets/scripts/HudScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HudScript : MonoBehaviour {
+
+	public void reset()
+    {
+        Debug.Log("reset");
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
