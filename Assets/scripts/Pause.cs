01c47460e1f389b90268dfc59e236d3b48bb79f2
Albert Tom <shadowseer99@aol.com>
2015-12-09 15:09:14 -0700

    added pause screen

---
+++ Assets/scripts/Pause.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+    [SerializeField] private GameObject screen;
+
+    private bool activated = false;
+    
+    // Use this for initialization
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bool reset = Input.GetKey(KeyCode.R);
+        bool menu = Input.GetKey(KeyCode.Escape);
+        if (reset)
+        {
+            Reset();
+        }
+        
+        if (menu)
+        {
+            if(!activated)
+            {
+                Time.timeScale = 0;
+                activated = true;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                activated = false;
+            }
+        }
+        screen.SetActive(activated);
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        activated = false;
+    }
+    public void Reset()
+    {
+        Time.timeScale = 1;
+        Application.LoadLevel(Application.loadedLevel);
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        Application.LoadLevel("Main Menu");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
