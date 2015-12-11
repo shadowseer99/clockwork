e7e0b4679622d642d5ab2cb1d6d2a277c256e066
Steven Petersen <spetersen216@gmail.com>
2015-12-11 00:32:43 -0700

    Added Cursor, Adjusted StretchMaterial, Fixed a Level

--- Assets/scripts/StretchMaterial.cs
+++ Assets/scripts/StretchMaterial.cs
@@ -8,9 +8,11 @@ public class StretchMaterial : MonoBehaviour {
 	public void Awake() {
 		Mesh mesh = GetComponent<MeshFilter>().mesh;
 		Vector2[] uvs = mesh.uv;
+		float x = transform.TransformVector(Vector3.right.normalized).magnitude;
+		float y = transform.TransformVector(Vector3.up.normalized).magnitude;
 		Vector2 mult = new Vector2(
-			Mathf.Max(1, transform.localScale.x/transform.localScale.y),
-			Mathf.Max(1, transform.localScale.y/transform.localScale.x));
+			Mathf.Max(1, x/y),
+			Mathf.Max(1, y/x));
 		for (int i=0; i<uvs.Length; ++i) {
 			uvs[i] = offset + ratio*new Vector2(mult.x*uvs[i].x, mult.y*uvs[i].y);
 		}
