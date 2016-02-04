985204f71617699c911fa7e398b1e0f7f30858f6
Albert Tom <shadowseer99@aol.com>
2016-02-04 16:43:34 -0700

    wall builder update

--- Assets/Editor/WallBuilderEditor.cs
+++ Assets/Editor/WallBuilderEditor.cs
@@ -16,6 +16,10 @@ public class WallBuilderEditor : Editor
         {
             myScript.BuildObject();
         }
+        if (GUILayout.Button("Kill Children"))
+        {
+            myScript.KillChildren();
+        }
         SceneView.RepaintAll();
     }
 }
--- Assets/scripts/tools/WallBuilderScript.cs
+++ Assets/scripts/tools/WallBuilderScript.cs
@@ -9,13 +9,28 @@ public class WallBuilderScript : MonoBehaviour {
     public GameObject w3h3;
     public GameObject w4h3;
     public GameObject w16h2;
-    public int width=2;
-    public int height=2;
+    public int width=1;
+    public int height=1;
 
     private GameObject[] blockArray;
 
     private GameObject[,] field;
     private int[,] idents;
+
+    public void KillChildren()
+    {
+        for (int w = 0; w < width; w++)
+        {
+            for (int h = 0; h < height; h++)
+            {
+                if(idents[w,h]!=-1)
+                {
+                    DestroyImmediate(field[w, h]);
+                    idents[w, h] = -1;
+                }
+            }
+        }
+    }
 
     public void BuildObject()
     {
@@ -27,8 +42,8 @@ public class WallBuilderScript : MonoBehaviour {
         {
             for (int h = 0; h < height; h++)
             {
-                int rand = Random.Range(0, 5);
-                //int rand = 4;
+                int rand = Random.Range(0, 7);
+                //int rand = 6;
                 if (rand == 0)
                 {
                     Vector3 v = new Vector3(.5f+ w + transform.position.x - width / 2f, h + transform.position.y - height / 2f, 0);
@@ -64,6 +79,22 @@ public class WallBuilderScript : MonoBehaviour {
                 else if (rand == 4)
                 {
                     Vector3 v = new Vector3(1.5f + w + transform.position.x - width / 2f, +h + transform.position.y - height / 2f, 0);
+                    field[w, h] = Instantiate(blockArray[rand]) as GameObject;
+                    field[w, h].transform.position = v;
+                    field[w, h].transform.parent = transform;
+                    idents[w, h] = rand;
+                }
+                else if (rand == 5)
+                {
+                    Vector3 v = new Vector3(2f + w + transform.position.x - width / 2f, +h + transform.position.y - height / 2f, 0);
+                    field[w, h] = Instantiate(blockArray[rand]) as GameObject;
+                    field[w, h].transform.position = v;
+                    field[w, h].transform.parent = transform;
+                    idents[w, h] = rand;
+                }
+                else if (rand == 6)
+                {
+                    Vector3 v = new Vector3(8f + w + transform.position.x - width / 2f, +h + transform.position.y - height / 2f, 0);
                     field[w, h] = Instantiate(blockArray[rand]) as GameObject;
                     field[w, h].transform.position = v;
                     field[w, h].transform.parent = transform;
@@ -248,12 +279,92 @@ public class WallBuilderScript : MonoBehaviour {
                         }
                     }
                 }
-
-
-                //test
-
-
-
+                else if (idents[w, h] == 5)
+                {
+                    if (h + 3 > height || w + 4 > width)
+                    {
+                        DestroyImmediate(field[w, h]);
+                        Vector3 v = new Vector3(1.5f + w + transform.position.x - width / 2f, h + transform.position.y - height / 2f, 0);
+                        field[w, h] = Instantiate(blockArray[4]) as GameObject;
+                        field[w, h].transform.position = v;
+                        field[w, h].transform.parent = transform;
+                        idents[w, h] = 4;
+                        h--;
+                    }
+                    else
+                    {
+                        for (int w2 = 0; w2 < 4; w2++)
+                        {
+                            for (int h2 = 0; h2 < 3; h2++)
+                            {
+                                if (w2 + h2 != 0)
+                                {
+                                    if (idents[w + w2, h + h2] == -1)
+                                    {
+                                        DestroyImmediate(field[w, h]);
+                                        Vector3 v = new Vector3(1.5f + w + transform.position.x - width / 2f, h + transform.position.y - height / 2f, 0);
+                                        field[w, h] = Instantiate(blockArray[4]) as GameObject;
+                                        field[w, h].transform.position = v;
+                                        field[w, h].transform.parent = transform;
+                                        idents[w, h] = 4;
+                                        h--;
+                                        w2 = 4;
+                                        h2 = 3;
+                                    }
+                                    else
+                                    {
+                                        DestroyImmediate(field[w + w2, h + h2]);
+                                        field[w + w2, h + h2] = null;
+                                        idents[w + w2, h + h2] = -1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (idents[w, h] == 6)
+                {
+                    if (h + 2 > height || w + 16 > width)
+                    {
+                        DestroyImmediate(field[w, h]);
+                        Vector3 v = new Vector3(2f + w + transform.position.x - width / 2f, h + transform.position.y - height / 2f, 0);
+                        field[w, h] = Instantiate(blockArray[5]) as GameObject;
+                        field[w, h].transform.position = v;
+                        field[w, h].transform.parent = transform;
+                        idents[w, h] = 5;
+                        h--;
+                    }
+                    else
+                    {
+                        for (int w2 = 0; w2 < 16; w2++)
+                        {
+                            for (int h2 = 0; h2 < 2; h2++)
+                            {
+                                if (w2 + h2 != 0)
+                                {
+                                    if (idents[w + w2, h + h2] == -1)
+                                    {
+                                        DestroyImmediate(field[w, h]);
+                                        Vector3 v = new Vector3(2f + w + transform.position.x - width / 2f, h + transform.position.y - height / 2f, 0);
+                                        field[w, h] = Instantiate(blockArray[5]) as GameObject;
+                                        field[w, h].transform.position = v;
+                                        field[w, h].transform.parent = transform;
+                                        idents[w, h] = 5;
+                                        h--;
+                                        w2 = 16;
+                                        h2 = 2;
+                                    }
+                                    else
+                                    {
+                                        DestroyImmediate(field[w + w2, h + h2]);
+                                        field[w + w2, h + h2] = null;
+                                        idents[w + w2, h + h2] = -1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
 
             }
         }
