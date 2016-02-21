cc64415baa58410c996508cadcef0b1e80cf3e62
Albert Tom <shadowseer99@aol.com>
2016-02-21 09:48:54 -0700

    music and menu

---
+++ Assets/scripts/MenuPan.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuPan : MonoBehaviour {
+
+    public int ymin = -174;
+    public int ymax = 155;
+    public int xmin = -204;
+    public int xmax = 214;
+    public float speed = 1;
+    private Vector3 direction = new Vector3(1, 1).normalized;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        this.transform.Translate(direction * speed * Time.deltaTime);
+        if(transform.position.x>xmax||transform.position.x<xmin)
+        {
+            direction.x=direction.x * -1;
+        }
+        if (transform.position.y > ymax || transform.position.y < ymin)
+        {
+            direction.y = direction.y * -1;
+        }
+    }
+}
