efdf6e38e05122b546d7d7e44e6a4048d45a7b28
Albert Tom <shadowseer99@aol.com>
2016-02-25 19:05:13 -0700

    fade in

---
+++ Assets/scripts/FadeIn.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeIn : MonoBehaviour
+{
+    public float fadetime;
+    public GameObject panel;
+    private Image img;
+    private float timer;
+    private Color fade = Color.black;
+
+    void Start()
+    {
+        panel.SetActive(true);
+        img = panel.GetComponent<Image>();
+        timer = fadetime;
+    }
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        fade.a =  (timer/fadetime);
+        img.color = fade;
+        if(timer<=0)
+        {
+            Destroy(this);
+        }
+    }
+
+
+}
