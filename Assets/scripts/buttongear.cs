318366a3463791b4ca2cb0943422be7535444bd6
Albert Tom <shadowseer99@aol.com>
2016-03-10 18:05:02 -0700

    spinning gears on main menu

---
+++ Assets/scripts/buttongear.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttongear : MonoBehaviour {
+
+    public GameObject gear;
+    private Animator ani;
+	// Use this for initialization
+	void Start () {
+        ani = gear.GetComponent<Animator>();
+	}
+	
+	public void Enter()
+    {
+        ani.SetBool("State", true);
+    }
+    public void Exit()
+    {
+        ani.SetBool("State", false);
+    }
+}
