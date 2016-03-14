cc1ef16bd48e01bf82d7898e298dea2487cb6758
Albert Tom <shadowseer99@aol.com>
2016-03-14 11:44:12 -0600

    music is in and properly works now

--- Assets/scripts/MusicObject.cs
+++ Assets/scripts/MusicObject.cs
@@ -3,34 +3,84 @@ using System.Collections;
 
 public class MusicObject : MonoBehaviour {
 	static bool exists;
-    public AudioClip defaultmusic;
-    public AudioClip watermusic;
+    public AudioSource defaultmusic;
+    public AudioSource momentummusic;
+    public AudioSource watermusic;
+    public AudioSource buttonmusic;
+    public AudioSource xtrememusic;
+    public int momstart;
+    public int momend;
     public int waterstart;
     public int waterend;
+    public int buttstart;
+    public int buttend;
+    public int xtrstart;
+    public int xtrend;
     private int playing=0;
-    private AudioSource music;
+    
     void Start()
     {
-        music = gameObject.GetComponent<AudioSource>();
+        
+    }
+    void OnLevelWasLoaded(int level)
+    {
+        if (level <= momend && level >= momstart && playing != 1)
+        {
+            defaultmusic.Stop();
+            momentummusic.Play();
+            watermusic.Stop();
+            buttonmusic.Stop();
+            xtrememusic.Stop();
+            playing = 1;
+        }
+        else if (level <= waterend && level >= waterstart && playing != 2)
+        {
+            defaultmusic.Stop();
+            momentummusic.Stop();
+            watermusic.Play();
+            buttonmusic.Stop();
+            xtrememusic.Stop();
+            playing = 2;
+        }
+        else if (level <= buttend && level >= buttstart && playing != 3)
+        {
+            defaultmusic.Stop();
+            momentummusic.Stop();
+            watermusic.Stop();
+            buttonmusic.Play();
+            xtrememusic.Stop();
+            playing = 3;
+        }
+        else if (level <= xtrend && level >= xtrstart && playing != 4)
+        {
+            defaultmusic.Stop();
+            momentummusic.Stop();
+            watermusic.Stop();
+            buttonmusic.Stop();
+            xtrememusic.Play();
+            playing = 4;
+        }
+        else if (level == 0)
+        {
+            defaultmusic.Play();
+            momentummusic.Stop();
+            watermusic.Stop();
+            buttonmusic.Stop();
+            xtrememusic.Stop();
+            playing = 0;
+        }
     }
     void Update()
     {
-        if(Application.loadedLevel<=waterend&&Application.loadedLevel>=waterstart&&playing!=2)
-        {
-            music.Stop();
-            music.clip = watermusic;
-            playing = 2;
-            music.Play();
-        }
-        else if(playing!=0)
-        {
-            music.Stop();
-            music.clip = defaultmusic;
-            playing = 0;
-            music.Play();
-        }
-        music.volume = SoundProfile.music;
+        
+        defaultmusic.volume = SoundProfile.music;
+        momentummusic.volume = SoundProfile.music;
+        watermusic.volume = SoundProfile.music;
+        buttonmusic.volume = SoundProfile.music;
+        xtrememusic.volume = SoundProfile.music;
+
         AudioListener.volume = SoundProfile.master;
+        transform.position = GameObject.FindGameObjectWithTag("MainCamera").transform.position;
     }
 	// Use this for initialization
 	void Awake()
