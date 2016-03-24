71dabc5b6cb4ad86a56719b8b8621da0e7dfeb2d
Albert Tom <shadowseer99@aol.com>
2016-03-24 10:31:33 -0600

    new pause menu

---
+++ Assets/scripts/menustuff/LevelIndicator.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelIndicator : MonoBehaviour {
+
+    public Sprite[] backings;
+    public Sprite[] solonum;
+    public Sprite[] ones;
+    public Sprite[] tens;
+    public GameObject first;
+    public GameObject second;
+    private Image tile;
+    private Image one;
+    private Image ten;
+
+
+    // Use this for initialization
+    void OnLevelWasLoaded (int level)
+    {
+        tile = gameObject.GetComponent<Image>();
+        one=first.GetComponent<Image>();
+        ten=second.GetComponent<Image>();
+        if(level>=1&&level<=16)
+        {
+            tile.sprite = backings[0];
+        }
+        else if (level >= 17 && level <= 30)
+        {
+            tile.sprite = backings[1];
+        }
+        else if(level >= 31 && level <= 45)
+        {
+            tile.sprite = backings[2];
+        }
+        else if (level >= 46 && level <= 50)
+        {
+            tile.sprite = backings[3];
+        }
+
+        if(level<10)
+        {
+            one.sprite = solonum[level];
+            one.color = Color.white;
+        }
+        else
+        {
+            one.color = Color.white;
+            ten.color = Color.white;
+            int a = level / 10;
+            int b = level % 10;
+            one.sprite = ones[a];
+            ten.sprite = tens[b];
+
+        }
+
+
+
+    }
+	
+	
+}
