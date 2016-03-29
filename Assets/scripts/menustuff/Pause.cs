1afd37d77990883524f56a2b18a19003ee96016b
Albert Tom <shadowseer99@aol.com>
2016-03-29 10:19:46 -0600

    level banner drop

--- Assets/scripts/menustuff/Pause.cs
+++ Assets/scripts/menustuff/Pause.cs
@@ -11,13 +11,15 @@ public class Pause : MonoBehaviour {
     [SerializeField] private GameObject options;
     [SerializeField] private float fadeTime=1;
 	[SerializeField] private float fadeDelay=1;
+    [SerializeField] private GameObject endBanner;
     private float timer=0;
 	private List<Image> images=new List<Image>();
 	private float timeSpent=0;
 	public Sprite[] numbers;
 	public RectTransform timeObject;
-
     private bool activated = false;
+    private bool dropping = false;
+    private float timedelay=0;
     
     // Use this for initialization
 
@@ -41,69 +43,94 @@ public class Pause : MonoBehaviour {
 	}
     void Update()
     {
-		timeSpent += Time.deltaTime;
+        if(dropping&&endBanner.transform.position.y>0)
+        {
+            endBanner.transform.Translate(0, -500 * Time.unscaledDeltaTime, 0);
+            if(Time.realtimeSinceStartup-timedelay>2)
+            {
+                loadnext();
+            }
+        }
+        else if(!dropping)
+        {
+            timeSpent += Time.deltaTime;
 
-		// handle fade
-		if (levelend.activeSelf)
-		{
-			timer = Mathf.Min(timer+Time.unscaledDeltaTime, fadeTime);
-			for (int i=0; i<images.Count; ++i)
-			{
-				Color c = images[i].color;
-				images[i].color = new Color(c.r, c.g, c.b, Mathf.Max(timer/fadeTime, 0));
-			}
-			Text t = timeObject.GetComponent<Text>();
-			t.color = new Color(t.color.r, t.color.b, t.color.b, Mathf.Max(timer/fadeTime, 0));
-			Image img = levelend.GetComponent<Image>();
-			img.color = new Color(img.color.r, img.color.g, img.color.b, Mathf.Min(Mathf.Max((timer+fadeDelay)/fadeTime, 0), 1));
-		}
+            // handle fade
+            if (levelend.activeSelf)
+            {
+                timer = Mathf.Min(timer + Time.unscaledDeltaTime, fadeTime);
+                for (int i = 0; i < images.Count; ++i)
+                {
+                    Color c = images[i].color;
+                    images[i].color = new Color(c.r, c.g, c.b, Mathf.Max(timer / fadeTime, 0));
+                }
+                Text t = timeObject.GetComponent<Text>();
+                t.color = new Color(t.color.r, t.color.b, t.color.b, Mathf.Max(timer / fadeTime, 0));
+                Image img = levelend.GetComponent<Image>();
+                img.color = new Color(img.color.r, img.color.g, img.color.b, Mathf.Min(Mathf.Max((timer + fadeDelay) / fadeTime, 0), 1));
+            }
 
 
 #if UNITY_IPHONE || UNITY_ANDROID
         bool reset = Mobile.reset;
         bool menu = Mobile.menu;
 #else
-        // Read the inputs.
-        bool reset = CrossPlatformInputManager.GetButtonDown("Reset");
-        bool menu = CrossPlatformInputManager.GetButtonDown("Pause");
+            // Read the inputs.
+            bool reset = CrossPlatformInputManager.GetButtonDown("Reset");
+            bool menu = CrossPlatformInputManager.GetButtonDown("Pause");
 #endif
 
-        if (reset)
-        {
-            Reset();
-        }
-        
-        if (menu)
-        {
-            if(!activated)
+            if (reset)
             {
+                Reset();
+            }
+
+            if (menu)
+            {
+                if (!activated)
+                {
 #if UNITY_IPHONE || UNITY_ANDROID
                 
                 Mobile.menu=false;
                 Mobile.active = false;
 #endif
-                Time.timeScale = 0;
-                activated = true;
-            }
-            else
-            {
+                    if (!dropping)
+                    {
+                        Time.timeScale = 0;
+                    }
+
+                    activated = true;
+                }
+                else
+                {
 #if UNITY_IPHONE || UNITY_ANDROID
             
                 Mobile.active = true;
 #endif
-                Time.timeScale = 1;
-                activated = false;
+                    Time.timeScale = 1;
+                    activated = false;
+                }
             }
+            screen.SetActive(activated);
         }
-        screen.SetActive(activated);
+        
+		
     }
 	public void PrintText() { print("Text"); }
 	public void NextLevel()
 	{
+        dropping = true;
 		print("next level...");
-		Time.timeScale = 1;
-		Application.LoadLevel (Application.loadedLevel + 1);
+
+        timedelay = Time.realtimeSinceStartup;
+        
 	}
+
+    private void loadnext()
+    {
+        Time.timeScale = 1;
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
     public void Resume()
     {
         Time.timeScale = 1;
