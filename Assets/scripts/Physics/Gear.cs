56f759bbe10171b18e3fe8393db3f5aabef57506
Steven Petersen <spetersen216@gmail.com>
2016-03-30 20:46:53 -0600

    Added Level End and Kickback sounds

--- Assets/scripts/Physics/Gear.cs
+++ Assets/scripts/Physics/Gear.cs
@@ -8,11 +8,14 @@ using System.Collections.Generic;
 public class Gear:CollidingObject {
 	public bool isGolden=false;
 	private float goldenRotation=0;
+	private float soundCounter=0;
 	
 	public AudioCustom _insert;
 	public AudioCustom _goldenRotating;
+	public AudioCustom _goldenRotated;
 	private AudioSource insert;
 	private AudioSource goldenRotating;
+	private AudioSource goldenRotated;
 
 	public override void Start() {
 		base.Start();
@@ -25,6 +28,9 @@ public class Gear:CollidingObject {
 		goldenRotating = gameObject.AddComponent<AudioSource>();
 		goldenRotating.clip = _goldenRotating.clip;
 		goldenRotating.loop = false;
+		goldenRotated = gameObject.AddComponent<AudioSource>();
+		goldenRotated.clip = _goldenRotated.clip;
+		goldenRotated.loop = false;
 	}
 
 	public override void Update() {
@@ -36,18 +42,27 @@ public class Gear:CollidingObject {
 		// handle goldenGear
 		if (isGolden) {
 			if (Application.isPlaying) {
-				if (!goldenRotating.isPlaying) goldenRotating.Play();
-				goldenRotating.volume = _goldenRotating.volume*SoundProfile.effects*Mathf.Abs(AngularVelocityToCurSpeed()/maxSpeed);
-				goldenRotating.pitch = 1+Mathf.Abs(goldenRotation)/360;
+				//if (!goldenRotating.isPlaying) goldenRotating.Play();
+				if (Mathf.Floor(Mathf.Abs(goldenRotation)/40) != Mathf.Floor(Mathf.Abs(goldenRotation+Time.fixedDeltaTime*curAngularVelocity)/40))
+					goldenRotating.PlayDelayed(0.1f);
+				goldenRotating.volume = 1;//_goldenRotating.volume*SoundProfile.effects*Mathf.Abs(AngularVelocityToCurSpeed()/maxSpeed);
+				goldenRotating.pitch = SemitonesToPitch(Mathf.Abs(goldenRotation)*24/360-12);
+				//print("pitch: "+goldenRotating.pitch);
 			}
 			goldenRotation += Time.fixedDeltaTime*curAngularVelocity;
 			if (Mathf.Abs(goldenRotation) > 360) {
+				goldenRotated.volume = _goldenRotated.volume*SoundProfile.effects;
+				goldenRotated.Play();
 				GameObject.FindObjectOfType<Pause>().EndLevel();
 				return;
 			}
 		}
 
 		base.PhysicsUpdate();
+	}
+
+	private float SemitonesToPitch(float semitones) {
+		return Mathf.Pow(2, semitones/12);
 	}
 }
 
--- Assets/scripts/Physics/Kickback2D.cs
+++ Assets/scripts/Physics/Kickback2D.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 public class Kickback2D:MonoBehaviour {
 	public enum KickbackMode { speed, energy }
 	public KickbackMode kickbackMode;
+	public AudioCustom _hitSound;
+	private AudioSource hitSound;
 	public float mult=1;
+
+	public void Start() {
+		hitSound = gameObject.AddComponent<AudioSource>();
+		hitSound.clip = _hitSound.clip;
+		hitSound.loop = false;
+	}
 	
-	void OnCollisionExit2D(Collision2D coll) {
-		if (kickbackMode==KickbackMode.speed)
-			coll.rigidbody.velocity *= mult;
-		else if (kickbackMode==KickbackMode.energy)
-			coll.rigidbody.velocity *= Mathf.Sqrt(mult);
-		else
-			throw new Exception("Invalid KickbackMode");
+	public void OnCollisionExit2D(Collision2D coll) {
+		if (coll.gameObject.GetComponent<CollidingObject>()!=null) {
+			hitSound.Play();
+			hitSound.volume = _hitSound.volume*SoundProfile.effects;
+
+			if (kickbackMode==KickbackMode.speed)
+				coll.rigidbody.velocity *= mult;
+			else if (kickbackMode==KickbackMode.energy)
+				coll.rigidbody.velocity *= Mathf.Sqrt(mult);
+			else
+				throw new Exception("Invalid KickbackMode");
+		}
 	}
 }
