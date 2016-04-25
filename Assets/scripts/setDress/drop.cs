797f8458e8c6266102a1d04d7dfbd6078e030412
Albert Tom <shadowseer99@aol.com>
2016-04-25 17:56:15 -0600

    water dropper

---
+++ Assets/scripts/setDress/drop.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drop : MonoBehaviour {
+    public GameObject ani;
+    public SpriteRenderer spr;
+    public Rigidbody2D body;
+    private bool hit = false;
+    private float time = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(hit)
+        {
+            time += Time.deltaTime;
+            if(time>.333)
+            {
+               Destroy(gameObject);
+            }
+        }
+	}
+    void OnCollisionEnter2D()
+    {
+        ani.SetActive(true);
+        spr.enabled = false;
+        body.gravityScale = 0;
+        hit = true;
+    }
+}
---
+++ Assets/scripts/setDress/droppletSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class droppletSpawner : MonoBehaviour {
+
+    public GameObject dropplet;
+    public int frequency = 5;
+    private float time = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        time += Time.deltaTime;
+        if(Random.Range(0,1000)<time*frequency)
+        {
+            time = 0;
+            Instantiate(dropplet, transform.position, Quaternion.identity);
+        }
+	}
+}
